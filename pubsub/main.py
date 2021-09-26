5450746109a8b068e3477b804f20fae12a0f2f98
Shingo <kawanos@google.com>
2021-09-27 00:50:53 +0900

    to notify something from Cloud Deploy to Slack channel

---
+++ pubsub/main.py
@@ -0,0 +1,31 @@
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+import os
+# TODO(developer)
+project_id = os.environ.get("PROJECT_ID")
+subscription_id = os.environ.get("SUBSC")
+# Number of seconds the subscriber should listen for messages
+# timeout = 5.0
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+import requests
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    requests.post("https://api.uname.link/slack", data={"message":message})
+    print(f"Received {message}.")
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done.
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result(timeout=timeout)
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
